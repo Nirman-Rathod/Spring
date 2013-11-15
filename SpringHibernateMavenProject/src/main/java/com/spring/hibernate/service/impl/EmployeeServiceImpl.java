40f856853b61c73baac5d6554a03040b1543e79a
Nirman-Rathod <nirman_rathod@yahoo.com>
2013-11-15 13:52:07 +0530

    Update nov 15, 2013

--- SpringHibernateMavenProject/src/main/java/com/spring/hibernate/service/impl/EmployeeServiceImpl.java
+++ SpringHibernateMavenProject/src/main/java/com/spring/hibernate/service/impl/EmployeeServiceImpl.java
@@ -3,8 +3,6 @@ package com.spring.hibernate.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
